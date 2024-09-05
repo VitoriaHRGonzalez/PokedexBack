c6ef5bc6842715ea82be4ba4d8205c9df00e4a1f
VitoriaGonzalez <vitoria.gonzalez@poatek.com>
2024-09-05 14:42:59 -0300

    modificações app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TrainerModule } from './trainer/trainer.module';
@@ -8,7 +9,10 @@ console.log('MongoDB URI:', process.env.MONGODB_URI); // Adiciona o console.log 
 
 
 @Module({
-  imports: [PokemonModule, TrainerModule, MongooseModule.forRoot(process.env.MONGODB_URI || "mongodb://localhost:27017/PokedexDB")],
+  imports: [PokemonModule, TrainerModule,
+    ConfigModule.forRoot(), // Carrega variáveis de ambiente do arquivo .env
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/PokedexDB'),
+  ],
   controllers: [],
   providers: [],
 })
