2a1db5e9d6f540fcbf41a217ce5747ddb6037192
VitoriaGonzalez <vitoria.gonzalez@poatek.com>
2024-09-05 11:45:44 -0300

    Adicionado log para depurar a variável de ambient

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TrainerModule } from './trainer/trainer.module';
 
+console.log('MongoDB URI:', process.env.MONGODB_URI); // Adiciona o console.log para verificar a variável de ambiente
+
+
 @Module({
   imports: [PokemonModule, TrainerModule, MongooseModule.forRoot(process.env.MONGODB_URI || "mongodb://localhost:27017/PokedexDB")],
   controllers: [],
